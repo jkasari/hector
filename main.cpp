72123f59d01a8c272cdb047912251ef63d0f1fde
Jason <jkasari@outlook.com>
2021-10-16 19:21:05 -0700

    Downloaded and linking both mnist and eigen libraries:w

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "../mnist/include/mnist/mnist_reader.hpp"
+#include "../eigen/Eigen/Dense"
 
 using namespace std;
+
+auto dataset = mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>();
 
 int main() {
 
