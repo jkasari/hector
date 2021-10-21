41bd9c0b3713f11c925122cb23fe784235e627e1
Jason <jkasari@outlook.com>
2021-10-20 21:27:20 -0700

    I don't know what I'm doing.
    The whole point of this project is to better understand the logic of
    neural networks. And I'm realizing how needed that is. This is really
    just turning into a mess of code with no real structer.

--- logic.hpp
+++ logic.hpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <math.h>
+#include <random>
 #include "eigen/Eigen/Dense"
 
-using namespace Eigen;
- 
-void eigenTest(std::vector<double> cVec) {
-  double* ptr = &cVec[0];
-  Eigen::Map<Eigen::VectorXd> vec(ptr, 784);
+// Rows then Colomns!!!
 
+
+void eigenTest(std::vector<double> cVec) {
+  Eigen::VectorXd vec(784);
+  for (int i = 0; i < 784; ++i) {
+    vec(i) = cVec[i];
+  }
   vec *= .1;
   double temp = vec.sum() -2750;
   std::cout << temp << std::endl;
--- main.cpp
+++ main.cpp
@@ -15,8 +15,44 @@ int main() {
   ReadMNISTImages("train-images-idx3-ubyte", 60000, 784, trainImages);
   ReadMNISTLabels("t10k-labels-idx1-ubyte", 10000, testLabels);
   ReadMNISTLabels("train-labels-idx1-ubyte", 60000, trainLabels);
+  static const uint16_t ILS = 784; // Input Layer Size
+  static const uint16_t HLS = 100; // Hidden Layer Size
+  static const uint16_t OLS = 10; // Output Layer Size
+  Eigen::VectorXd HiddenLayerInput = Eigen::VectorXd::Random(ILS);
+  Eigen::MatrixXd HiddenLayerWeights = Eigen::MatrixXd::Random(HLS, ILS);
+  Eigen::VectorXd HiddenLayerBias = Eigen::VectorXd::Random(HLS);
+  Eigen::VectorXd HiddenLayerOutPut = Eigen::VectorXd::Random(HLS);
+  Eigen::MatrixXd OutPutLayerWeights = Eigen::MatrixXd::Random(OLS, HLS);
+  Eigen::VectorXd OutPutLayerBias = Eigen::VectorXd::Random(OLS);
 
-  eigenTest(trainImages[0]);
+  for (int i = 0; i < ILS; ++i) {
+    HiddenLayerInput(i) = trainImages[0][i];
+  }
+
+  //int index = 9;
+  //for (int i = 0; i < 784; ++i) {
+  //  if (trainImages[index][i] < 100) {
+  //    cout << " ";
+  //  }
+  //  if (trainImages[index][i] < 10) {
+  //    cout << " ";
+  //  }
+  //  cout << trainImages[index][i];
+  //  if (i % 28 == 0) {
+  //    cout << endl;
+  //  }
+  //}
+
+
+  Eigen::MatrixXd m(3, 4);
+  m << 1, 2, 3, 4,
+       5, 6, 7, 8,
+       9, 1, 2, 3;
+  cout << m << endl;
+  Eigen::Vector4d v(1, 2, 3, 4);
+  cout << m*v << endl;
+  //cout << HiddenLayerInput << endl;
+
 
   return 0;
 }
